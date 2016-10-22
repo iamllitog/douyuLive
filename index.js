ca8d7cd216f5916cd93e0fcdddd26eae08ea2b17
iamllitog <2212986228@qq.com>
2016-10-22 21:53:06 +0800

    修改首次异常未删除播放源问题

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ if(!chapter){
 }
 
 function errorHandle(err) {
-	del([`${__dirname}/data`]);
+	del.sync([`${__dirname}/data`]);
 	logger.error("遇到异常，删除缓存文件，中断进程");
 	logger.error(err);
 	process.exit();
